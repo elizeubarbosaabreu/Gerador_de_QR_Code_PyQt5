ecbdfe4cc05f452a83cec91176e47a9587b39f24
Elizeu Barbosa Abreu <elizeubarbosaabreu@yahoo.com>
2023-12-30 13:27:14 -0400

    app add

---
+++ README.md
@@ -0,0 +1,30 @@
+# GERADOR DE QR CODE PyQt5
+
+
+## O QUE É?
+
+![image.png](/images/captura_de_tela.png)
+
+O **Gerador de QR Code PyQt5** é um aplicativo desenhado no [Qt Designer](https://www.qt.io) e programado em Python usando PyQt5 e tem por finalidade converter links ou textos simples em qr code...
+
+## COMO USAr?
+
+Basta baixar o repositório com o comando: `link do repo` e começar a usar o app abrindo um terminal dentro do diretório e usando o comando: `python3 app.py`
+
+As dependências podem ser instaladas via pip usando o comando: `pip install -r requirements.txt`
+
+Caso baixou o app para testes e modificações, talvez seja necessário criar um ambiente virtual de desenvolvimento:
+
+~~~ python
+python3 -m venv venv
+source venv/bin/activate
+pip install -r requirements.txt
+~~~
+
+Aí para executar o programa basta rodar o comando: `python3 app.py`.
+
+
+## aGRADECIMENTOS
+
+Obrigado por visitar meu repositório e acreditar no meu trabalho.
+
---
+++ app.py
@@ -0,0 +1,46 @@
+from PyQt5 import uic, QtWidgets, QtGui
+import qrcode
+
+
+def gerar_e_exibir_imagem(): 
+    content = janela.lineEdit.text() 
+    imagem_qrcode = qrcode.make(content)
+    imagem_qrcode.save("image.png")   
+    
+    carregar_imagem()    
+    
+
+def salvar_img():
+    
+    content = janela.lineEdit.text()
+    imagem_qrcode = qrcode.make(content)
+    imagem_qrcode.save("image.png")
+    
+    carregar_imagem() 
+    
+    janela.lineEdit.setText('')  # Limpar campo  
+
+    arquivo = QtWidgets.QFileDialog.getSaveFileName()[0]
+    
+    imagem_qrcode.save(arquivo)    
+    
+def carregar_imagem():
+    imagem = QtGui.QPixmap('image.png')  # Carregar a imagem
+    cena = QtWidgets.QGraphicsScene()  # Criar uma cena
+    item = QtWidgets.QGraphicsPixmapItem(imagem)  # Criar um item com a imagem
+    cena.addItem(item)  # Adicionar o item à cena
+    janela.graphicsView.setScene(cena)  # Definir a cena na graphicsView
+
+
+
+
+app = QtWidgets.QApplication([])
+janela = uic.loadUi("template.ui")
+
+
+janela.pushButton.clicked.connect(gerar_e_exibir_imagem)
+janela.pushButton_2.clicked.connect(salvar_img)
+
+
+janela.show()
+app.exec()
